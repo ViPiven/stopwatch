53f1eb039db7f5ede9d786070bc7d59d82344de8
ViPiven <vitalijjpiven777@gmail.com>
2021-06-27 00:07:13 +0300

    Add RxJs

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Demo
+[DEMO LINK](https://ViPiven.github.io/stopwatch/)
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
 import './App.scss';
 import {useEffect, useState} from "react";
-import { Observable } from "rxjs";
+import { interval } from 'rxjs';
+
+const source = interval(1000);
+let subscribe;
+let working = false;
 
 function App() {
   const [time, setTime] = useState(0)
   const [timerOn, setTimerOn] = useState(false)
+    const timerStart = () => {
+      if(!working) {
+          subscribe = source.subscribe(val => {
+              setTime(currentTime => currentTime + 1);
+          })
+          working = true;
+      }
+    }
 
-  let wasClicked = false;
-  let timeout;
+    let wasClicked = false;
+    let timeout;
     const handleClick = () => {
+        if (wasClicked) {
+            subscribe.unsubscribe();
+            working = false;
+        }
         wasClicked = true;
         timeout = setTimeout(() => {
             wasClicked = false;
-            setTimerOn(false);
         }, 300);
+
     }
 
-    useEffect(() => {
-        let interval = null;
-        if(timerOn) {
-            interval = setInterval(() => {
-                setTime(prevTime => prevTime + 10)
-            }, 10)
-        } else {
-            clearInterval(interval)
-        }
+  useEffect(() => {
+       subscribe = source.subscribe(val => {
+          setTime(currentTime => currentTime + 1);
+      });
 
-        return () => clearInterval(interval)
-    }, [timerOn])
+      if (!timerOn) {
+          subscribe.unsubscribe();
+          working = false;
+      }
+  }, [timerOn])
 
   return (
     <div className="App">
       <div className="time">
-          <span className="time__value">{("0" + Math.floor((time / 3600000) % 60)).slice(-2)}:</span>
-          <span className="time__value">{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-          <span className="time__value">{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+          <span className="time__value">{("0" + Math.floor(((time / 3600)) % 60)).slice(-2)}:</span>
+          <span className="time__value">{("0" + Math.floor((time / 60) % 60)).slice(-2)}:</span>
+          <span className="time__value">{("0" + Math.floor((time) % 60)).slice(-2)}</span>
       </div>
-      <div className="buttons">
-          <button onClick={() => setTimerOn(true)} className="buttons__start">Start</button>
-          <button onClick={() => setTimerOn(false)} className="buttons__stop">Stop</button>
-          <button onClick={handleClick} className="buttons__wait">Wait</button>
-          <button onClick={() => setTime(0)} className="buttons__reset">Reset</button>
-      </div>
+        <div className="buttons">
+            <button onClick={timerStart} className="buttons__start">Start</button>
+            <button onClick={() => {subscribe.unsubscribe(); working = false}} className="buttons__stop">Stop</button>
+            <button onClick={handleClick} className="buttons__wait">Wait</button>
+            <button onClick={() => setTime(0)} className="buttons__reset">Reset</button>
+        </div>
     </div>
   );
 }
